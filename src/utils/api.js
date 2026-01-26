b8cb538a18a4d361d0955542d75c339307fa4d0c
hasan-nazmul <nh2826239@gmail.com>
2026-01-26 06:51:26 +0600

    updated to tailwind css

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,20 +1,12 @@
+// src/utils/api.js
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:8000/api', // Your Laravel API
+    // If VITE_API_URL exists, use it. Otherwise use localhost.
+    baseURL: import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000/api',
     headers: {
+        'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'Accept': 'application/json'
     }
 });
-
-// Automatically add the token to every request if it exists
-api.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-});
-
-export default api;
+// ... rest of your code
